dc42af87929376d0226727d8bb325ec42edd6185
NyBeata <78854516+NyBeata@users.noreply.github.com>
2022-11-30 11:33:09 +0100

    Add files via upload

---
+++ I40FDC_1130/JSONParseI40FDC/src/i40fdc/ObjectI40FDC.java
@@ -0,0 +1,24 @@
+package i40fdc;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
+
+public class ObjectI40FDC {
+
+	public static void main(String[] args) {
+
+		JSONObject obj = new JSONObject();
+		
+		obj.put(nev, "Nyíri Beáta" );
+		obj.put(neptunkod, "I40FDC" );
+		obj.put(szak, "GE-BP" );
+
+	}
+
+}
